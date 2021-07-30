81ded84e59fb4fe15eddfb18ca549bef3b87cce4
KyleWalkerley <walkerleykyle@gmail.com>
2021-07-30 12:02:40 +0200

    30/07/2021

--- HerbSchiltdOricleExercises/src/main/java/Chapter5/Excersise5_1.java
+++ HerbSchiltdOricleExercises/src/main/java/Chapter5/Excersise5_1.java
@@ -12,7 +12,7 @@ package Chapter5;
 public class Excersise5_1 {
     public static void main(String args[]) { 
     double nums[] = { 1.1, 2.2, 3.3, 4.4, 5.5, 
-                      6.6, 7.7, 8.8, 9.9, 10.1 }; 
+                      6.6, 7.7, 8.8, 9.9, 10.10 }; 
     double sum = 0; 
  
     for(int i=0; i < nums.length; i++) 
